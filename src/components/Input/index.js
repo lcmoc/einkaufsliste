79dbcebb85b2fa1ccfe8d0dfa9ef3470feb80994
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-12-02 15:02:55 +0100

    header finished

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,13 @@
+import trash from "../../assets/trash.png";
+
 const Input = () => {
   return (
-    <div className="MainConatainer">
-      <input type="text" className="mb-3 ml-5" />
-      <button className="w-4 h-4 bg-gray-200">+</button>
+    <div className="flex items-start justify-start">
+      <input type="text" className="mb-3 ml-5 w-52" />
+      <button className="w-6 h-6 bg-gray-200 ml-1">+</button>
+      <button>
+        <img src={trash} alt="trashcan" className="ml-20 w-6 h-6" />
+      </button>
     </div>
   );
 };
